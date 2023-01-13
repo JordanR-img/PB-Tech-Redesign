39c85ec46da8398e777305ef542796c677a1bd01
JordanR-img <jordanrobin18@gmail.com>
2023-01-13 21:51:47 +1300

    making guest sign in page

---
+++ NodeJS/Frontend/pbt/src/Pages/PBTGuestSignIn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Style from "../Styling/GuestSignIn.module.css";
+import TopNav from "../components/LoginTopNav";
+import Footer from "../components/LoginFooter";
+export default function PBTGuestSignIn() {
+  return (
+    <div>
+      <TopNav />
+
+      <Footer />
+    </div>
+  );
+}
